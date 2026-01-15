d9af3ae83955618f2fcf3d12e2d34f0b3062defd
kihiku <gimdodam0520@gmail.com>
2026-01-15 19:49:29 +0900

    feafure secrion

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,4 @@
-/** header change effect **/
 
-const header = document.querySelector('#header')
-const stickyHeader = () => {
-    const scry = window.scrollY;
-    if (scry > 0) {
-        header.classList.add('active');
-    } else {
-        header.classList.remove('active');
-    }
-};
-
-window.addEventListener('scroll', stickyHeader);
 
 /** scroll reveal effect**/
 const sr = ScrollReveal({
@@ -29,7 +17,7 @@ sr.reveal('.meet-text-box', {
     distance: '49px',
 });
 
-sr.reveal('.meet-wrapper img', {
+sr.reveal('.meet-wrapper img, .feature', {
     duration: 1000,
     origin: 'bottom',
     distance: '40px',
