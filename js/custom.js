b24c82e559894fd3d4b9624d8d1ea829c207696b
kihiku <gimdodam0520@gmail.com>
2026-01-16 18:53:23 +0900

    2026.01.16

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,3 @@
-
-
 /** scroll reveal effect**/
 const sr = ScrollReveal({
     reset: false,
@@ -23,3 +21,23 @@ sr.reveal('.meet-wrapper img, .feature', {
     distance: '40px',
     interval: 200, //0.2초 간격으로 효과 실행
 });
+
+/** Swiper Slider Effect**/
+setTimeout(() => {
+    const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+}, 300);
---
+++ js/request.js
@@ -0,0 +1,52 @@
+async function getRequest(url) {
+    return await fetch(url).then((response) => {
+        if(!response.ok) {
+            throw new Error('Error: ' + response.status);
+        }
+        return response.json();
+    }
+    )
+}
+
+
+const sliderWrapper = document.querySelector('.swiper-wrapper');
+const offerWrapper = document.querySelector('.products');
+// const siderDOM = `<div class="swiper-slide">
+//                 <div class="slider-image">
+//                     <img src="images/${pro_img}" alt="slider image">
+//                 </div>
+//                 <div class="slider-text">
+//                     <h4>${pro_name}</h4>
+//                     <p>${pro_desc}</p>
+//                     <a href="#" class="common-button">자세히 보기</a>
+//                 </div>
+//              </div>`
+
+async function getProducts(n, wrapper) {
+    const getProductUrl = `https://www.dabipyeung.com/soaply_backend/model/get_products.php?qnt=${n}`;
+
+    try {// 요청 시도
+        const data = await getRequest(getProductUrl);
+        let dataElement = '';
+
+        data.map((item) => {
+            const {pro_img, pro_name,pro_desc} = item
+            dataElement += `<div class="swiper-slide">
+                <div class="slider-image">
+                    <img src="images/${pro_img}" alt="slider image">
+                </div>
+                <div class="slider-text">
+                    <h4>${pro_name}</h4>
+                    <p>${pro_desc}</p>
+                    <a href="#" class="common-button">자세히 보기</a>
+                </div>
+             </div>`
+        });
+
+        wrapper.insertAdjacentHTML('beforeend',dataElement);
+    } catch (error) {// 요청 시 에러 사항
+        console.log(`error: ${error}`);
+    }
+}
+
+getProducts(4, sliderWrapper)
